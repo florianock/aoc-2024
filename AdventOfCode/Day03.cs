3dddad3dca0d71d7550b56a2d25d6f864e3e8b03
Florian <florian@zocalo-ict.nl>
2024-12-03 11:51:31 +0100

    day 3, use one regex only

--- AdventOfCode/Day03.cs
+++ AdventOfCode/Day03.cs
@@ -15,14 +15,14 @@ public partial class Day03 : BaseDay
         // _input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
     }
 
-    public override ValueTask<string> Solve_1() => new($"{RunProgram(InstructionRegex())}"); // Test: 161
+    public override ValueTask<string> Solve_1() => new($"{RunProgram(true)}"); // Test: 161
 
-    public override ValueTask<string> Solve_2() => new($"{RunProgram(InstructionRegexWithConditionals())}"); // Test: 48
+    public override ValueTask<string> Solve_2() => new($"{RunProgram()}"); // Test: 48
 
-    private int RunProgram(Regex regex)
+    private int RunProgram(bool ignoreConditionals = false)
     {
         var result = 0;
-        var program = regex.Matches(_input);
+        var program = InstructionRegex().Matches(_input);
         var isEnabled = true;
         foreach (Match line in program)
         {
@@ -32,7 +32,7 @@ public partial class Day03 : BaseDay
                 case "do()": isEnabled = true; break;
                 case "don't()": isEnabled = false; break;
                 default:
-                    if (isEnabled)
+                    if (ignoreConditionals || isEnabled)
                     {
                         result += instruction.Substring(4, instruction.Length - 5)
                             .Split(',')
@@ -44,9 +44,6 @@ public partial class Day03 : BaseDay
         return result;
     }
     
-    [GeneratedRegex(@"mul\(-?[0-9]+,-?[0-9]+\)")]
-    private static partial Regex InstructionRegex();
-
     [GeneratedRegex(@"don't\(\)|do\(\)|mul\(-?[0-9]+,-?[0-9]+\)")]
-    private static partial Regex InstructionRegexWithConditionals();
+    private static partial Regex InstructionRegex();
 }
