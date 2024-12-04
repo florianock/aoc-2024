19f2789b727febd8376ca9961e973a7f14298725
Florian <florian@zocalo-ict.nl>
2024-12-04 16:36:44 +0100

    day 4, simpler is faster

--- AdventOfCode/Day04.cs
+++ AdventOfCode/Day04.cs
@@ -5,67 +5,79 @@
 /// </summary>
 public sealed class Day04 : BaseDay
 {
-    private readonly Dictionary<(int, int), char> _input;
+    private readonly string[] _input;
 
     public Day04()
     {
-        var lines = File.ReadLines(InputFilePath).ToArray();
-        // _lines = "MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX".Split('\n');
+        _input = File.ReadLines(InputFilePath).ToArray();
+        // _input = "MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX".Split('\n');
         // Test1: 18
         // Test2: 9
-        _input = new Dictionary<(int, int), char>();
-        for (var r = 0; r < lines.Length; r++)
-        {
-            for (var c = 0; c < lines[0].Length; c++)
-            {
-                _input.Add((r, c), lines[r][c]);
-            }
-        }
     }
 
     public override ValueTask<string> Solve_1() => new($"{SearchForXmas()}");
 
-    public override ValueTask<string> Solve_2() => new($"{SearchForRealXmas()}");
+    public override ValueTask<string> Solve_2() => new($"{SearchForCrossMas()}");
 
     private int SearchForXmas()
     {
         const string pattern = "XMAS";
         var result = 0;
-        foreach (var point in _input.Where(kvp => kvp.Value == pattern[0]))
+        var maxRow = _input.Length - 1;
+        var maxCol = _input[0].Length - 1;
+        for (var r = 0; r <= maxRow; r++)
         {
-            foreach (var direction in Enum.GetValues<Direction>())
+            for (var c = 0; c <= maxCol; c++)
             {
-                var matchedCharacters = 1;
-                foreach (var m in Enumerable.Range(1, 3))
+                if (_input[r][c] != pattern[0]) continue;
+                foreach (var direction in Enum.GetValues<Direction>())
                 {
-                    if (_input.GetValueOrDefault(Step(point.Key, direction, m), '.') != pattern[m]) break;
-                    matchedCharacters++;
-                }
+                    var matchedCharacters = 1;
+                    foreach (var m in Enumerable.Range(1, 3))
+                    {
+                        var (nextRow, nextCol) = Step((r, c), direction, m);
+                        if (nextRow < 0 || maxRow < nextRow ||
+                            nextCol < 0 || maxCol < nextCol ||
+                            _input[nextRow][nextCol] != pattern[m])
+                        {
+                            break;
+                        }
 
-                if (matchedCharacters == 4) result++;
+                        matchedCharacters++;
+                    }
+
+                    if (matchedCharacters == 4) result++;
+                }
             }
         }
 
         return result;
     }
 
-    private int SearchForRealXmas()
+    private int SearchForCrossMas()
     {
         var result = 0;
-        foreach (var point in _input)
+        var maxRow = _input.Length - 1;
+        var maxCol = _input[0].Length - 1;
+        for (var r = 1; r <= maxRow - 1; r++)
         {
-            if (point.Value != 'A') continue;
-            var (r, c) = point.Key;
-            (int, int)[] diagonalCoordinates =
-            [
-                Step((r, c), Direction.NorthWest), Step((r, c), Direction.NorthEast),
-                Step((r, c), Direction.SouthWest), Step((r, c), Direction.SouthEast)
-            ];
-            var diagonalChars = diagonalCoordinates.Select(p => _input.GetValueOrDefault(p, '.')).ToArray();
-            if (diagonalChars.Count(i => i == 'M') == 2 && diagonalChars.Count(i => i == 'S') == 2 &&
-                diagonalChars[1] != diagonalChars[2])
+            for (var c = 1; c <= maxCol - 1; c++)
             {
-                result += 1;
+                if (_input[r][c] != 'A') continue;
+                (int, int)[] diagonalCoordinates =
+                [
+                    Step((r, c), Direction.NorthWest), Step((r, c), Direction.NorthEast),
+                    Step((r, c), Direction.SouthWest), Step((r, c), Direction.SouthEast)
+                ];
+                var diagonalChars = diagonalCoordinates
+                    .Select(point => _input[point.Item1][point.Item2])
+                    .ToArray();
+                if (diagonalChars.Count(i => i == 'M') == 2
+                    && diagonalChars.Count(i => i == 'S') == 2
+                    && diagonalChars[1] != diagonalChars[2])
+                {
+                    result++;
+                }
             }
         }
 
