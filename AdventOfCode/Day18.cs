5d94ce76104253d4bae40c7b1712690da07fa0c4
Florian <florian@zocalo-ict.nl>
2024-12-18 15:23:22 +0100

    Day 18 small refactor

--- AdventOfCode/Day18.cs
+++ AdventOfCode/Day18.cs
@@ -13,10 +13,10 @@ public class Day18 : BaseDay
 
     public Day18()
     {
-        var input = File.ReadLines(InputFilePath).ToList();
-        // var input =
-            // "5,4\n4,2\n4,5\n3,0\n2,1\n6,3\n2,4\n1,5\n0,6\n3,3\n2,6\n5,1\n1,2\n5,5\n2,5\n6,5\n1,4\n0,4\n6,4\n1,1\n6,1\n1,0\n0,5\n1,6\n2,0"
-                // .Split("\n").ToList();
+        // var input = File.ReadLines(InputFilePath).ToList();
+        var input =
+            "5,4\n4,2\n4,5\n3,0\n2,1\n6,3\n2,4\n1,5\n0,6\n3,3\n2,6\n5,1\n1,2\n5,5\n2,5\n6,5\n1,4\n0,4\n6,4\n1,1\n6,1\n1,0\n0,5\n1,6\n2,0"
+                .Split("\n").ToList();
         _width = input.Count < 30 ? 7 : 71;
         _height = input.Count < 30 ? 7 : 71;
         _time = input.Count < 30 ? 12 : 1024;
@@ -42,6 +42,13 @@ public class Day18 : BaseDay
 
     public override ValueTask<string> Solve_2() => new($"{GetCutOffPoint()}"); // Test: 6,1
 
+    private HashSet<Point> PathToEnd()
+    {
+        var start = new Point(0, 0);
+        var end = new Point(_width - 1, _height - 1);
+        return AStar(start, end, _grid, ManhattanDistance);
+    }
+
     private string GetCutOffPoint()
     {
         var path = PathToEnd();
@@ -58,41 +65,32 @@ public class Day18 : BaseDay
         return "-1,-1";
     }
 
-    private HashSet<Point> PathToEnd()
+    private static HashSet<Point> AStar(Point start, Point goal, char[][] grid, Func<Point, Point, int> h)
     {
-        var start = new Point(0, 0);
-        var end = new Point(_width - 1, _height - 1);
-        return AStar(start, end, ManhattanDistance);
-    }
-
-    private HashSet<Point> AStar(Point start, Point goal, Func<Point, Point, int> h)
-    {
-        var openSet = new HashSet<Point>();
+        var frontier = new PriorityQueue<Point, int>();
         var cameFrom = new Dictionary<Point, (Point, int)>();
         var gScore = new Dictionary<Point, int> { { start, 0 } };
         var fScore = new Dictionary<Point, int> { { start, h(start, goal) } };
-        openSet.Add(start);
+        frontier.Enqueue(start, 0);
 
-        while (openSet.Count > 0)
+        while (frontier.Count > 0)
         {
-            var current = openSet.Select(p => (p, fScore[p])).OrderBy(p => p.Item2).First().Item1;
-            openSet.Remove(current);
+            var current = frontier.Dequeue();
             if (current.Equals(goal)) return ReconstructPath(goal);
             foreach (var neighbor in GetNeighbors(current).Where(n =>
-                         0 <= n.Y && n.Y < _grid.Length &&
-                         0 <= n.X && n.X < _grid[0].Length &&
-                         _grid[n.Y][n.X] != '#').ToList())
+                         0 <= n.Y && n.Y < grid.Length &&
+                         0 <= n.X && n.X < grid[0].Length &&
+                         grid[n.Y][n.X] != '#').ToList())
             {
                 var tentativeGScore = gScore[current] + 1; // d(current, neighbor)
                 if (gScore.TryGetValue(neighbor, out var value) && tentativeGScore >= value) continue;
                 gScore[neighbor] = tentativeGScore;
-                if (cameFrom.TryGetValue(neighbor, out _))
-                    cameFrom[neighbor] = (current, tentativeGScore);
-                else cameFrom.Add(neighbor, (current, tentativeGScore));
+                cameFrom[neighbor] = (current, tentativeGScore);
                 if (fScore.ContainsKey(neighbor))
                     fScore[neighbor] = tentativeGScore + h(neighbor, goal);
                 else fScore.Add(neighbor, tentativeGScore + h(neighbor, goal));
-                openSet.Add(neighbor);
+                // actually should skip if frontier already contains neighbor
+                frontier.Enqueue(neighbor, fScore[neighbor]);
             }
         }
 
@@ -115,6 +113,7 @@ public class Day18 : BaseDay
 
     private static void Draw(char[][] maze, HashSet<Point> path = null)
     {
+        Console.WriteLine();
         for (var y = 0; y < maze.Length; y++)
         {
             for (var x = 0; x < maze[0].Length; x++)
