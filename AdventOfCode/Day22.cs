d3bcd0f916c253147259ad01b70ff15f7159b0d5
Florian <florian@zocalo-ict.nl>
2024-12-22 17:13:03 +0100

    Day 22 done

--- AdventOfCode/Day22.cs
+++ AdventOfCode/Day22.cs
@@ -9,33 +9,73 @@ public sealed class Day22 : BaseDay
 
     public Day22()
     {
-        _input = File.ReadLines(InputFilePath);
+        _input = File.ReadLines(InputFilePath); // 1909 too low
         // _input = "1\n10\n100\n2024".Split('\n'); // part 1
         // _input = "1\n2\n3\n2024".Split('\n'); // part 2
+        // _input = "123".Split('\n'); // part 2
     }
 
-    public override ValueTask<string> Solve_1() => new($"{_input.AsParallel().Sum(n => IterateSecretNumbers(int.Parse(n), 2000))}"); // Test: 37327623
+    public override ValueTask<string> Solve_1() =>
+        new($"{_input.AsParallel().Sum(n => GetFinalSecretNumber(int.Parse(n)))}"); // Test: 37327623
 
-    public override ValueTask<string> Solve_2() => new($"{_input.Sum(n => IterateSecretNumbers(int.Parse(n), 2000, true))}"); // Test: 23
+    public override ValueTask<string> Solve_2() => new($"{_input
+        .AsParallel()
+        .Aggregate(new Dictionary<(int, int, int, int), long>(), (agg, n) => Merge(agg, GetChangesAndBananas(int.Parse(n))))
+        .Max(n => n.Value)}"); // Test: 23
 
-    private static long IterateSecretNumbers(int number, int iterations, bool part2 = false)
+    private static long GetFinalSecretNumber(int number) =>
+        Enumerable.Range(0, 2000).Aggregate((long)number, (current, _) => GetNextSecretNumber(current));
+
+    private static Dictionary<(int, int, int, int), long> GetChangesAndBananas(int number)
     {
-        if (part2) return 0; // -2,1,-1,3
-        var result = (long)number;
-        for (var i = 0; i < iterations; i++)
+        const int iterations = 2000;
+        var changes = new Dictionary<(int, int, int, int), long>();
+        var previousPrice = GetPrice(number);
+        var previousChanges = (0, 0, 0, 0);
+        var currentSecret = (long)number;
+        for (var i = 0; i < iterations - 1; i++)
         {
-            result = GetNextSecretNumber(result);
+            currentSecret = GetNextSecretNumber(currentSecret);
+            var price = GetPrice(currentSecret);
+            var (_, a, b, c) = previousChanges;
+            var newChange = (a, b, c, price - previousPrice);
+            previousPrice = price;
+            if (i >= 3)
+            {
+                changes[newChange] = changes.GetValueOrDefault(newChange, price);
+            }
+
+            previousChanges = newChange;
         }
 
-        // Console.WriteLine($"{number}: {result}");
-        return result;
+        // if (changes.TryGetValue((0, 5, 0, 0), out var value)) Console.WriteLine($"{number} => {value}.");
+        // else Console.WriteLine($"Not found for {number}.");
 
-        long GetNextSecretNumber(long n)
+        return changes;
+
+        int GetPrice(long n) => int.Parse(n.ToString()[^1..]);
+    }
+
+    private static long GetNextSecretNumber(long secretNumber)
+    {
+        secretNumber = Prune(Mix(secretNumber * 64L, secretNumber));
+        secretNumber = Prune(Mix(secretNumber / 32L, secretNumber));
+        return Prune(Mix(secretNumber * 2048L, secretNumber));
+
+        long Mix(long n, long secret) => n ^ secret;
+        long Prune(long n) => n % 16777216L;
+    }
+
+    private static Dictionary<(int, int, int, int), long> Merge(
+        Dictionary<(int, int, int, int), long> a,
+        Dictionary<(int, int, int, int), long> b
+    )
+    {
+        return b.Aggregate(a, (agg, cur) =>
         {
-            n = ((n * 64L) ^ n) % 16777216L;
-            n = ((n / 32L) ^ n) % 16777216L;
-            n = ((n * 2048L) ^ n) % 16777216L;
-            return n;
-        }
+            if (agg.TryGetValue(cur.Key, out var value)) agg[cur.Key] += cur.Value;
+            else agg[cur.Key] = cur.Value;
+            return agg;
+        });
     }
 }
