ca9627af877d72a04dfec2330e8efea6188a5b40
Florian <florian@zocalo-ict.nl>
2024-12-22 12:03:45 +0100

    Day 22 first part

---
+++ AdventOfCode/Day22.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode;
+
+/// <summary>
+/// --- Day 22: Monkey Market ---
+/// </summary>
+public sealed class Day22 : BaseDay
+{
+    private readonly IEnumerable<string> _input;
+
+    public Day22()
+    {
+        _input = File.ReadLines(InputFilePath);
+        // _input = "1\n10\n100\n2024".Split('\n'); // part 1
+        // _input = "1\n2\n3\n2024".Split('\n'); // part 2
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{_input.AsParallel().Sum(n => IterateSecretNumbers(int.Parse(n), 2000))}"); // Test: 37327623
+
+    public override ValueTask<string> Solve_2() => new($"{_input.Sum(n => IterateSecretNumbers(int.Parse(n), 2000, true))}"); // Test: 23
+
+    private static long IterateSecretNumbers(int number, int iterations, bool part2 = false)
+    {
+        if (part2) return 0; // -2,1,-1,3
+        var result = (long)number;
+        for (var i = 0; i < iterations; i++)
+        {
+            result = GetNextSecretNumber(result);
+        }
+
+        // Console.WriteLine($"{number}: {result}");
+        return result;
+
+        long GetNextSecretNumber(long n)
+        {
+            n = ((n * 64L) ^ n) % 16777216L;
+            n = ((n / 32L) ^ n) % 16777216L;
+            n = ((n * 2048L) ^ n) % 16777216L;
+            return n;
+        }
+    }
+}
